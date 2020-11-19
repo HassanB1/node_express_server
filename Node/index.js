2e9f55a4d7031628ca7ffece4d66749161153748
HassanB1 <hassanb1@aetna.com>
2020-11-19 13:05:01 -0500

    SERVE A REACT APP VIA DOCKER AND EXPRESS NODE SERVER

--- Node/index.js
+++ Node/index.js
@@ -5,7 +5,7 @@ const port = process.env.NODE_PORT || 9000;
 
 
 app.use(express.static(path.join(__dirname,'../my-app/', 'build')));
-app.get('/', function(req,res)){
+app.get('/', function(req,res){
         res.sendFile(path.join(__dirname,'../my-app/', 'build', 'index.html'));
 });
 
